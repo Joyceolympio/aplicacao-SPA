b1d9e37c013327917d66da081152ceb7acbfec83
marka <marka@LAPTOP-INOCT3S4>
2020-04-21 22:02:12 -0300

    Criação do site completa

---
+++ aplicacao/src/app/contato/contato.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
+@Component({
+  selector: 'app-contato',
+  templateUrl: './contato.component.html',
+  styleUrls: ['./contato.component.css']
+})
+export class ContatoComponent implements OnInit {
+
+  faFacebook = faFacebook
+  faInstagram = faInstagram
+  faWhatsapp = faWhatsapp
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
